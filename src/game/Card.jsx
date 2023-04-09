b0992d7beac1316c2e4ae259e00bfc2c7990144c
Javiera Saavedra <javiera.saavedra@uc.cl>
2023-04-09 00:26:22 -0400

    feat(board): add useEffect and useContext

--- src/game/Card.jsx
+++ src/game/Card.jsx
@@ -1,21 +1,32 @@
 import './Card.css'
-import { useState } from 'react'
+import {useContext, useState} from 'react'
 import CardButton from './CardButton'
+import {GameContext} from "./Board.jsx";
 
-export default function Card({imgSrc}){
-    const [showImage, setShowImage] = useState(true);
+export default function Card({id, imgSrc}){
+  const [showImage, setShowImage] = useState(true);
+  const {
+    guess,
+    setGuess
+  } = useContext(GameContext);
 
-    const toggleImage =() => {
-        setShowImage(!showImage);
-    }
+  const toggleImage = () => {
+    setShowImage(!showImage);
+  }
 
+  const handleGuess = () => {
+    setGuess(id);
+  }
 
-    return(
-        <div className="card">
-            <div className="card-container">
-                {showImage && <img src={imgSrc} className="img" alt="avatar"/>}
-            </div>
-            <CardButton onClick={toggleImage} showImage={showImage}/>
-        </div>
-    )
+  return(
+    <div className="card">
+      <div className="card-container">
+        {showImage && <img src={imgSrc} className="img" alt="avatar"/>}
+      </div>
+      <div className="card-actions">
+        <CardButton onClick={toggleImage} label={showImage ? "Ocultar" : "Mostrar"}/>
+        {showImage && <CardButton onClick={handleGuess} label={"Adivinar"}/>}
+      </div>
+    </div>
+  )
 }
